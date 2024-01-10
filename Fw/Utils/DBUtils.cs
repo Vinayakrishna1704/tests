806b5a216b4d1a1095ce8a4454db392d64a423fb
Madhavan070603 <20ec088@kpriet.ac.in>
2024-01-10 17:05:09 +0530

    added change

--- Fw/Utils/DBUtils.cs
+++ Fw/Utils/DBUtils.cs
@@ -6,7 +6,7 @@ namespace Fw.Utils
 
 	public static class DbUtils
 	{
-		private static readonly string connectionString = "Data Source=DESKTOP-9ICU8OK\\SQLEXPRESS;Initial Catalog=Apartment;Integrated Security=True";
+		private static readonly string connectionString = "Data Source=MADHAVAN\\SQLEXPRESS;Initial Catalog=Apartment;Integrated Security=True";
 
 		public static SqlConnection GetConnection()
 		{
